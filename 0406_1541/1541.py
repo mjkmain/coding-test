89420b9fdb5046f687eab08b3ea0014385d2e9b9
Kim min jun <72269271+mjkmain@users.noreply.github.com>
2023-04-05 01:28:02 +0900

    0406

---
+++ 0406_1541/1541.py
@@ -0,0 +1,19 @@
+input_str = input()
+
+def str_plus(plus_str):
+    return sum(list(map(int,plus_str.split("+"))))
+
+
+cur_sum = 0
+init = input_str.split("-")[0]
+
+if input_str.startswith("-"):
+    cur_sum -= str_plus(init)
+
+else:
+    cur_sum += str_plus(init)
+
+for d in input_str.split("-")[1:]:
+    cur_sum -= str_plus(d)
+
+print(cur_sum)

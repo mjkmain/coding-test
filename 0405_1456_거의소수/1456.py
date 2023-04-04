66e619cd282e35fa834f1b4d8804ca3416c856aa
Kim min jun <72269271+mjkmain@users.noreply.github.com>
2023-04-05 00:57:55 +0900

    0405

--- 0405_1456_거의소수/1456.py
+++ 0405_1456_거의소수/1456.py
@@ -1,24 +1,26 @@
 A, B = list(map(int, input().split()))
 
-prime_list = list(range(0, B+1))
+prime_list = list(range(0, int(B**0.5)+1))
 
 for i in range(2, int(B ** 0.5)+1):
     if prime_list[i] > 0:
-        for j in range(i+i, B+1, i):
+        for j in range(i+i, int(B**0.5)+1, i):
             prime_list[j] = -1
+
 
 prime_list[0] = -1
 prime_list[1] = -1
 
 count = 0
-for num in range(len(prime_list)):
-    cur_num  = prime_list[num]
-    if cur_num > 0:
+for prime_num in prime_list:
+    if prime_num > 0:
         i = 2
         while True:
             
-            if (cur_num**i <= B) and (cur_num**i >= A):
+            if (prime_num**i <= B) and (prime_num**i >= A):
                 count += 1
+                i += 1
+            elif (prime_num**i <= B) and not(prime_num**i >= A):
                 i += 1
             else:
                 break
